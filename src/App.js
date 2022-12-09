d9d43745b413a79bc4b8c400866600f6e2a99ec5
Don-Sebastian <donsebastian1333@gmail.com>
2022-12-10 00:38:22 +0530

    netflix update1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './Components/NavBar/Navbar';
+import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
+import {originals, action, trending, horrorMovies, romanceMovies, documentaries, comedyMovies} from './urls';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Banner />
+      <RowPost title="Netflix Originals" url={originals} />
+      <RowPost title="Trending " isSmall url={trending} />
+      <RowPost title="Actions" isSmall url={action} />
+      <RowPost title="Horror Movies" isSmall url={horrorMovies} />
+      <RowPost title="Romance Movies" isSmall url={romanceMovies} />
+      <RowPost title="Documentaries" isSmall url={documentaries} />
+      <RowPost title="Comedy Movies" isSmall url={comedyMovies} />
     </div>
   );
 }
